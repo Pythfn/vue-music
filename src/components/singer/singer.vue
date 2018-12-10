f10adc58ba05286a0a14279e28fa3952fec3f73f
Pythfn <rusonosy@outlook.com>
2018-12-10 22:49:50 +0800

    new singer List

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,17 +1,30 @@
 <template>
   <div class="singer">
-    geshou
+    <scroll class="bscroll">
+      <div>
+        <ul>
+          <ul v-for="item of singerList">
+            {{ item.title }}
+          </ul>
+        </ul>
+      </div>
+    </scroll>
   </div>
 </template>
+
 <script>
 import { getSingerList } from 'api/singer.js'
 import Singer from 'common/js/singer.js'
+import scroll from 'base/scroll/scroll'
 export default {
   data() {
     return {
       singerList: [],
       tempList: []
     }
+  },
+  components: {
+    scroll
   },
   created() {
     this._getSingerList()
@@ -27,26 +40,29 @@ export default {
     },
     formatList(list) {
       let hotSingerLength = 10
-      let newList = []
+      let newList = {
+        hot : {
+          title: '热门',
+          items: []
+        }
+      }
       let temp = []
-      let hotList = [{
-        title: '热门',
-        items: []
-      }]
-      hotList.items = []
+      newList.hot.items = []
       list.forEach((item) => {
         temp.push(new Singer(item.Fsinger_mid, item.Fsinger_name, item.Findex))
       })
       for (let i = 0; i < hotSingerLength; i++) {
-        hotList.items.push(temp[i])
+        newList.hot.items.push(temp[i])
       }
-      newList.push(hotList)
       temp.forEach((item) => {
         if (item.key.search(/^[a-zA-Z]$/) === 0) {
           if (!newList[item.key]) {
-            newList[item.key] = []
+            newList[item.key] = {
+              title: item.key,
+              items: []
+            }
           }
-          newList[item.key].push(item)
+          newList[item.key].items.push(item)
         }
       })
       return newList
