c0ff854146a2d2e7eb2082cde5c86a20074564f7
Pythfn <41435733+Pythfn@users.noreply.github.com>
2018-12-21 22:56:22 +0800

    singer-detail

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,24 +1,87 @@
 <template>
   <transition name="slide">
-    <div class="singer-detail">11111111111111111111111111111111111111111111111111111111111111111111</div>
+    <div class="singer-detail">
+      <div class="singer-title">
+        <div class="back">
+          <i class="icon-back"></i>
+        </div>
+        <div class="singerName">{{ getSinger.name}}</div>
+      </div>
+      <div>
+        <img :src="singerImage" class="singerImage">
+      </div>
+      <div class="song-list">
+        <ul>
+          <li v-for="item in data.list">{{ item.musicData.albumname }}</li>
+        </ul>
+      </div>
+    </div>
   </transition>
 </template>
 <script>
+import { mapGetters } from 'vuex'
+import { getSingerDetail } from 'api/singer'
 export default {
-  created (){
-    console.log(111)
+  data () {
+    return {
+      data: []
+    }
+  },
+  computed: {
+    singerImage() {
+      return this.getSinger.img
+    },
+    ...mapGetters([
+      'getSinger'
+    ])
+  },
+  mounted () {
+    getSingerDetail(this.getSinger.id).then((data) => {
+      if(data.subcode === 0) {
+        this.data = data.data
+        console.log(data)
+        console.log(this.data)
+      }
+    })
   }
 }
 </script>
 <style lang="stylus" scoped>
+@import '~common/stylus/variable.styl'
+
 .singer-detail
   position: absolute
-  top: 88px
+  top: 0
   bottom: 0
   left:0
   right:0
-  background: red
+  background: $color-background
   z-index: 99
+  .singer-title
+    position:absolute
+    top:0
+    left:0
+    right:0
+    .back
+      width:42px
+      height:42px
+      text-align:center
+      .icon-back
+        display: block
+        padding: 10px
+        font-size: $font-size-large-x
+        color: $color-theme
+    .singerName
+      position:absolute
+      top:0
+      left:0
+      right:0
+      bottom:0
+      text-align: center
+      font-size:22px
+      margin-top:10px
+  .singerImage
+    width:100%
 .slide-enter-active, .slide-leave-active
   transition: all 0.2s
 .slide-enter, .slide-leave-to
