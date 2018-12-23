78d2fffd51d9764fb9bce8a92d315cd259bd8c51
Pythfn <rusonosy@outlook.com>
2018-12-23 22:40:54 +0800

    singerdetail完成
    实现musiclist滚动缩放的特效

--- src/base/song-list/song-list.vue
+++ src/base/song-list/song-list.vue
@@ -27,7 +27,6 @@ export default {
 .songList
   width:100%
   height:100%
-  background: $color-background
   .song-item
     padding-top:25px
     padding-left:25px
--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -12,24 +12,41 @@
     <div
      class="singerBg"
      ref="singerBg"
+     :style="singerBgImg"
     >
-      <img :src="singerBg" class="singerBgimg">
-    </div>
-      <scroll
-       class="songScroll"
-       :data="songList"
-       :probetype="probetype"
-       :listenScroll="listenScroll"
-       @scroll="scroll"
-       ref="songScroll"
+      <div
+       class="playAllSongs"
+       v-show="data.length"
+       ref="playAllSongs"
       >
-        <div>
-          <song-list :songs="songList"></song-list>
+        <div class="playBtn">
+          <i class="icon-play"></i>
+          <span>随机播放全部</span>
         </div>
-      </scroll>
+      </div>
+      <div
+       class="singerBgTrans"
+       ref="singerBgTrans"
+      ></div>
     </div>
+    <div
+     class="scrollBg"
+     ref="scrollBg"
+    ></div>
+    <scroll
+     class="songScroll"
+     :data="songList"
+     :probetype="probetype"
+     :listenScroll="listenScroll"
+     @scroll="scroll"
+     ref="songScroll"
+    >
+      <div class="songList">
+        <song-list :songs="songList"></song-list>
+      </div>
+    </scroll>
+  </div>
 </template>
-
 <script>
 import SongList from 'base/song-list/song-list'
 import Scroll from 'base/scroll/scroll'
@@ -52,6 +69,11 @@ export default {
       default: ''
     }
   },
+  computed: {
+    singerBgImg() {
+      return `background-image:url(${this.singerBg})`
+    }
+  },
   data() {
     return {
       scrollY: ''
@@ -71,7 +93,7 @@ export default {
   },
   mounted() {
     this.singerBgHeight = this.$refs.singerBg.clientHeight
-    this.minScrollHeight = - this.singerBgHeight + SINGER_TITLE_HEIGHT
+    this.minScrollHeight = this.singerBgHeight - SINGER_TITLE_HEIGHT
     this.$refs.songScroll.$el.style.top = `${this.singerBgHeight}px`
     window.addEventListener('resize', () => {
       this.singerBgHeight = this.$refs.singerBg.clientHeight
@@ -84,33 +106,56 @@ export default {
   },
   watch: {
     scrollY(newY) {
-      //负数，返回最大的负数
-      this.scrollHeight = Math.max(this.minScrollHeight, newY)
-      this.thescrollHeight = this.singerBgHeight+this.scrollHeight
-      this.$refs.songScroll.$el.style.top = `${this.thescrollHeight}px`
+      //  负数，返回最大的负数
+      //  this.scrollHeight = Math.max(this.minScrollHeight, newY)
+      //  this.thescrollHeight = this.singerBgHeight+this.scrollHeight
+      //  this.$refs.songScroll.$el.style.top = `${this.thescrollHeight}px`
+      if (this.minScrollHeight - Math.abs(newY) > 0) {
+        this.$refs.scrollBg.style['transform'] = `translate3d(0, ${newY}px, 0)`
+        this.$refs.scrollBg.style['-webkit-transform'] = `translate3d(0, ${newY}px, 0)`
+        //  标题需要遮住列表的滚动
+        this.$refs.singerBg.style.zIndex = 0
+        this.$refs.singerBg.style.paddingBottom = '70%'
+        this.$refs.singerBgTrans.style.paddingBottom = '70%'
+        this.$refs.playAllSongs.style.display = ''
+      } else {
+        this.$refs.scrollBg.style['transform'] = `translate3d(0,${-this.minScrollHeight}px, 0)`
+        this.$refs.scrollBg.style['-webkit-transform'] = `translate3d(0,${-this.minScrollHeight}px, 0)`
+        this.$refs.singerBg.style.zIndex = 20
+        this.$refs.singerBg.style.paddingBottom = `${SINGER_TITLE_HEIGHT}px`
+        this.$refs.singerBgTrans.style.paddingBottom = `${SINGER_TITLE_HEIGHT}px`
+        this.$refs.playAllSongs.style.display = 'none'
+      }
+      //  下拉标题图缩放，使用transform的scale 2d缩放
+      let scaleNum = 1 + newY / 150
+      if (newY > 0) {
+        this.$refs.singerBg.style['transform'] = `scale(${scaleNum})`
+      } else {
+        this.$refs.singerBg.style['transform'] = `scale(1)`
+      }
     }
   }
 }
-</script>
 
+</script>
 <style lang="stylus" scoped>
 @import '~common/stylus/variable.styl'
 .singer-detail
-  position: absolute
+  //  必须使用fixed，使用absolute会出现滚动条
+  position: fixed
   top: 0
   bottom: 0
   left:0
   right:0
   background: $color-background
-  z-index: 99
   .singer-title
     position:absolute
     top:0
     left:0
     right:0
+    z-index: 50
     .back
       position:absolute
-      z-index: 50
       .icon-back
         display: block
         padding: 10px
@@ -126,18 +171,37 @@ export default {
       font-size:18px
       margin-top:5px
   .singerBg
+    position: relative
     width:100%
     height:0
     padding-bottom:70%
+    background-size: cover
     overflow:hidden
-    .singerBgimg
+    .singerBgTrans
+      background: rgba(7, 17, 27, 0.4)
       width:100%
+      height:0
+      padding-bottom:70%
+    .playAllSongs
+      position:absolute
+      bottom:15px
+      width:100%
+      .playBtn
+        border:1px solid $color-theme
+        padding:10px
+        border-radius: 15px
+        width:110px
+        margin:0 auto
+        color: $color-theme
+        font-size:12px
+        text-align:center
+  .scrollBg
+    background:$color-background
+    height:100%
   .songScroll
-    position:absolute
+    position: fixed
     top:0
     left:0
     right:0
     bottom:0
-    overflow:hidden
-
 </style>
